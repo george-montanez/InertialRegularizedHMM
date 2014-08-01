2e010282d477a95dd03cccaa71e9129246a78fcf
george-montanez <gmontane@cs.cmu.edu>
2014-08-01 11:03:01 -0400

    Changes to evaluation functions

--- evaluation.py
+++ evaluation.py
@@ -6,6 +6,7 @@ from GaussianHMM import GaussianHMM
 segment_func = GaussianHMM(1,np.random.random(size=(2,2))).get_segments
 
 def compute_cost_matrix(predicted_states, true_states):
+    predicted_states -= predicted_states.min()
     N = len(true_states)
     num_states = max(true_states) + 1
     cost = np.zeros((num_states, num_states))
@@ -19,6 +20,7 @@ def compute_cost_matrix(predicted_states, true_states):
     return cost            
 
 def segment_measurements(predicted_states, true_states):
+    predicted_states -= predicted_states.min()
     s1 = segment_func(predicted_states)
     s2 = segment_func(true_states)
     l1 = len(s1)
@@ -26,6 +28,7 @@ def segment_measurements(predicted_states, true_states):
     return l1/l2, max(l1, l2)/min(l1, l2), abs(l1-l2)
 
 def max_correct(predicted_states, true_states):
+    predicted_states -= predicted_states.min()
     mc = 0
     num_states = max(true_states) + 1
     best_perm = []
@@ -37,7 +40,8 @@ def max_correct(predicted_states, true_states):
         mc = max(mc, new_mc)     
     return mc, mc / true_states.shape[0], int(mc == len(true_states))
 
-def entropies(predicted_states, true_states):    
+def entropies(predicted_states, true_states):
+    predicted_states -= predicted_states.min()
     ps, ts = predicted_states, np.array(true_states, dtype="int8")
     K_p = np.max(ps) + 1
     K_t = np.max(ts) + 1
@@ -53,3 +57,10 @@ def entropies(predicted_states, true_states):
     H_pt = -sum([joint[j][k] * np.log2(joint[j][k]) for j in range(K_p) for k in range(K_t) if joint[j][k]])
     I_pt = sum([joint[j][k] * np.log2(joint[j][k] / (p_cluster_probs[j] * t_cluster_probs[k])) for j in range(K_p) for k in range(K_t) if joint[j][k]])
     return H_ps, H_ts, H_pt, I_pt
+
+def evaluate_prediction_set(predicted_states, true_states):
+    predicted_states -= predicted_states.min()
+    ents = entropies(predicted_states, true_states)
+    normed_voi = (ents[0] + ents[1] - 2 * ents[3]) / ents[2]
+    res = (max_correct(predicted_states, true_states), segment_measurements(predicted_states, true_states), normed_voi)
+    return res
